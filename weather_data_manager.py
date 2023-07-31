942fc88f8ca3e41d956197648d6932bee501539d
szabii70@stud.uni-obuda.hu <szabii70@stud.uni-obuda.hu>
2023-07-31 23:56:48 +0200

    api call written into isolated function

--- weather_data_manager.py
+++ weather_data_manager.py
@@ -14,16 +14,14 @@ class WeatherDataManager():
         '''
         The city name is being requested from the user until it gives an appropriate city name.
         '''
-        precipitation_list_byquarter = [0,0,0,0]
         valid_choice = False
         while valid_choice != True:
             user_input = input('Please give the name of the city for weather information (write "esc" to escape): ')
             if user_input.lower() == 'esc' : break
+
             self.city_name = user_input
             self.coords_bycity_url = f'http://api.openweathermap.org/geo/1.0/direct?q={self.city_name}&limit={self.limit}&appid={self.api_key}'
-
-            res = requests.get(self.coords_bycity_url)
-            location_data = json.loads(res.text)
+            location_data = self.fetch_api_synchronously(self.coords_bycity_url)
 
             if len(location_data) != 0:
                 self.lat = location_data[0]['lat']
@@ -41,9 +39,7 @@ class WeatherDataManager():
         This function is based on the lat, lon and api_key attributes, if these ones are missing, it can lead to an error.
         '''
         self.weather_data_url = f'https://api.openweathermap.org/data/3.0/onecall?lat={self.lat}&lon={self.lon}&appid={self.api_key}'
-
-        res = requests.get(self.weather_data_url)
-        weather_data = json.loads(res.text)
+        weather_data = self.fetch_api_synchronously(self.weather_data_url)
         try:
             weather_data_precipitation = weather_data['minutely']
             return weather_data_precipitation
@@ -57,7 +53,6 @@ class WeatherDataManager():
         try:
             precipitation_list = self.precipitation_amount()
             precipitation_list_byquarter = [0,0,0,0]
-
             quarter_counter = 0
 
             for index, precipitation_dic in enumerate(precipitation_list):
@@ -66,7 +61,12 @@ class WeatherDataManager():
                 #Every quarter of an hour is checked here. If the index is dividable by 15 it means 15 minutes passed by.
                 if index%15 == 0 and index != 0:
                     quarter_counter += 1
-            print(precipitation_list_byquarter) #This should be removed after aanouter function is made for displaying the data
             return precipitation_list_byquarter
         except:
             return [0,0,0,0]
+
+    def fetch_api_synchronously(self,url):
+        res = requests.get(url)
+        data = json.loads(res.text)
+
+        return data
--- weather_main.py
+++ weather_main.py
@@ -1,11 +1,20 @@
 from weather_data_manager import WeatherDataManager
 
+def data_display(data):
+	'''
+	This function requires a list as a parameter. The value of the parameter is displayed on the console.
+	'''
+	print('\n\nThe amount of precipitation in the next 1 hour:')
+	for index, value in enumerate(data):
+		print(f'\t{index+1}. quarter amount: {value}')
+
 def main():
 	wdm = WeatherDataManager()
 
 	while True:
 		if wdm.get_city_input() == 'esc' : break
-		wdm.segmented_precipitation_amount()
+		precipitation_data = wdm.segmented_precipitation_amount()
+		data_display(precipitation_data)
 	print('End of application')
 
 if __name__ == '__main__':
