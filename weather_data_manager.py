cd9607c9af64fc0e452a9d3fb13953a5188ba631
szabii70@stud.uni-obuda.hu <szabii70@stud.uni-obuda.hu>
2023-07-31 20:46:06 +0200

    coords can be fetched

--- weather_data_manager.py
+++ weather_data_manager.py
@@ -2,26 +2,29 @@ import requests
 import json
 
 class WeatherDataManager():
-	def __init__(self):
-		#This key is required for the API call, otherwise it will not work
-		self.api_key = '72fed8af3a02dd4950e5ff70ca29eb60'
-		self.lat = ''
-		self.lon = ''
-		self.city_name = ''
-		self.limit = 1
-		self.coords_bycity_url = f'http://api.openweathermap.org/geo/1.0/direct?q={city_name}&limit={limit}&appid={api_key}'
-		self.weather_data_url = f'https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&appid={api_key}'
 
-	def get_valid_city_input(self):
-		valid_choice = False
-		while valid_choice != True:
-			city_name = input('Please give the name of the city for weather information: ')
-			#try:
-			print(coords_bycity_url)
-			#res = requests.get(coords_bycity_url)
-			#location_data = json.loads(res.text)
-			print(location_data)
-			valid_choice = True
-			break
-			#except:
-				#print('There is no city with this name, please try again...')
+    def __init__(self):
+        self.api_key = '72fed8af3a02dd4950e5ff70ca29eb60'
+        self.lat = ''
+        self.lon = ''
+        self.city_name = ''
+        self.limit = 1
+        #self.weather_data_url = f'https://api.openweathermap.org/data/3.0/onecall?lat={self.lat}&lon={self.lon}&appid={self.api_key}'
+
+    def get_valid_city_input(self):
+        valid_choice = False
+        while valid_choice != True:
+            self.city_name = input('Please give the name of the city for weather information: ')
+            self.coords_bycity_url = f'http://api.openweathermap.org/geo/1.0/direct?q={self.city_name}&limit={self.limit}&appid={self.api_key}'
+
+            res = requests.get(self.coords_bycity_url)
+            location_data = json.loads(res.text)
+
+            if len(location_data) != 0:
+                print(location_data[0]['lat'])
+                print(location_data[0]['lon'])
+
+                valid_choice = True
+                break
+            else:
+                print('Invalid city name...')
