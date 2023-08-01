222b7664ab9a2b1651a5b4703f5465944c325a2d
szabii70@stud.uni-obuda.hu <szabii70@stud.uni-obuda.hu>
2023-08-01 13:03:25 +0200

    export function parameeter handling

--- data_exporter.py
+++ data_exporter.py
@@ -1,4 +1,12 @@
+import csv
+
 class DataExporter():
 
-	def ExportToCsv():
-		print('ExportToCsv')
+	def export_precipitation_datamodel_to_csv(precipitation_data_model):
+		'''
+		This function requires a PrecipitationDataModel as parameter
+		'''
+		if type(precipitation_data_model) == PrecipitationDataModel:
+			print('ExportToCsv')
+		else:
+			raise Exception('This function only accepts PrecipitationDataModel as parameter...')
