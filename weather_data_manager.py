06efc66e37d95b84563329f1d0ab675a0310e13c
szabii70@stud.uni-obuda.hu <szabii70@stud.uni-obuda.hu>
2023-08-01 11:59:52 +0200

    selfmade cache functions removed

--- weather_data_manager.py
+++ weather_data_manager.py
@@ -58,7 +58,6 @@ class WeatherDataManager():
         Gives the amount of precipitation of every quarter of the next hour. If the cache should be updated then the function sends
         an HTTP request, otherwise it will reuturn a cached data.
         '''
-        #if self.cache_update_needed():
         try:
             precipitation_list = self.precipitation_amount()
             precipitation_list_byquarter = [0,0,0,0]
@@ -70,41 +69,12 @@ class WeatherDataManager():
                 #Every quarter of an hour is checked here. If the index is dividable by 15 it means 15 minutes passed by.
                 if index%15 == 0 and index != 0:
                     quarter_counter += 1
-            #self.create_new_cache_record(precipitation_list_byquarter)
             return precipitation_list_byquarter
         except:
             return [0,0,0,0]
-        #else:
-            #print('Cached data is returned')
-            #return self.precipitation_cache[self.city_name.lower()]['precipitation_data']
 
     def fetch_api_synchronously(self,url):
         res = requests.get(url)
         data = json.loads(res.text)
 
         return data
-
-    def cache_update_needed(self, cache_time = 20):
-        '''
-        This function helps to decide if a new HTTP request is required or not. Cache_time parameter gives the interval
-        for long the data should be cached so it should be an integer. If there is no record of a certain city, the function should return True
-        so an HTTP request is needed. If there is already a record, then if the given time (eg. 20 min) did not pass
-        it should return False otherwise True. 
-        '''
-        try:
-            previous_date_time = self.precipitation_cache[self.city_name.lower()]['date_time']
-        except:
-            return True
-
-        current_date_time = datetime.now()
-        time_difference = current_date_time - previous_date_time
-        time_difference_in_seconds = time_difference.total_seconds()
-
-        if time_difference_in_seconds > int(cache_time)*60:
-            return True
-        else:
-            return False
-
-    def create_new_cache_record(self, precipitation_data):
-        print('New record is created')
-        self.precipitation_cache[self.city_name.lower()] = {'date_time' : datetime.now(), 'precipitation_data' : precipitation_data}
