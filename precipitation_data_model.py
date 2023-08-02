bf0c04ea8c3884f82323e646389c267623f1528b
szabii70@stud.uni-obuda.hu <szabii70@stud.uni-obuda.hu>
2023-08-02 13:14:22 +0200

    PrecipitationDataModel parameter checking

--- precipitation_data_model.py
+++ precipitation_data_model.py
@@ -1,6 +1,14 @@
+from datetime import datetime 
+
 class PrecipitationDataModel():
 
 	def __init__(self, city_name, precipitation_data, date_time):
-		self.city_name = city_name
-		self.precipitation_data = precipitation_data
-		self.date_time = date_time
+		'''
+		PrecipitationDataModel(str, [int/float], datetime)
+		'''
+		if type(city_name) == str and type(precipitation_data) == type([]) and type(date_time) == datetime:
+			self.city_name = city_name
+			self.precipitation_data = precipitation_data
+			self.date_time = date_time
+		else:
+			raise Exception('Thic class only accepts: str, [int=float], Date!')
